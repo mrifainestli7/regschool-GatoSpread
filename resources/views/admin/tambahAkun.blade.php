4f5f9f67661a831b2e7e1583f22950a88ecd6f0a
rifalfebiyan <rifalfebiyan123@gmail.com>
2024-04-27 16:17:15 +0700

    update tambah akun
    belum finall

--- resources/views/admin/tambahAkun.blade.php
+++ resources/views/admin/tambahAkun.blade.php
@@ -8,64 +8,65 @@
 
             <div class="container mt-5">
                 <div class="row justify-content-center">
-                    <div class="col-md-6">
+                    <div class="col-md-17">
                         <div class="card">
-                            <div class="card-header">
+                            <div class="card-header" style="background-color: #4c45b4; color: white;">
                                 Input data akun
                             </div>
-                            <div class="card-body">
+                            <div class="card-body" >
                                 
                                 <form action="" method="POST" enctype="multipart/form-data">
                                     @csrf
                                     <!-- NIP -->
-                                    <div class="form-group">
-                                        <label for="nip">NIP</label>
+                                    <div class="form-group mb-3">
+                                        <label for="nip">NIP*</label>
                                         <input type="number" class="form-control" id="nip" name="nip" value="{{ old('nip') }}" placeholder="Masukkan NIP" >
                                     </div>
                                     @error('nip')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
                                     <!-- Nama -->
-                                    <div class="form-group">
-                                        <label for="nama">Nama</label>
+                                    <div class="form-group mb-3">
+                                        <label for="nama">Nama*</label>
                                         <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" placeholder="Masukkan Nama" >
                                     </div>
                                     @error('nama')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
                                     <!-- Nomor Telepon -->
-                                    <div class="form-group">
-                                        <label for="nomor_telepon">Nomor Telepon</label>
+                                    <div class="form-group mb-3">
+                                        <label for="nomor_telepon">Nomor Telepon*</label>
                                         <input type="tel" class="form-control" id="nomor_telepon" name="nomor_telepon" value="{{ old('nomor_telepon') }}" placeholder="Masukkan Nomor Telepon" >
                                     </div>
                                     @error('nomor_telepon')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
                                     <!-- E-mail -->
-                                    <div class="form-group">
-                                        <label for="email">E-mail</label>
+                                    <div class="form-group mb-3">
+                                        <label for="email">E-mail*</label>
                                         <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Masukkan E-mail" >
                                     </div>
                                     @error('email')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
                                     <!-- Password -->
-                                    <div class="form-group">
-                                        <label for="password">Password</label>
+                                    <div class="form-group mb-3">
+                                        <label for="password">Password*</label>
                                         <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}" placeholder="Masukkan Password" >
                                     </div>
                                     @error('password')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
-                                    <div class="form-group">
-                                        <label for="picture">foto profil</label>
-                                        <input type="file" class="form-control" id="picture" name="picture">
+                                    <div class="form-group ">
+                                    <label for="picture">Foto Profil*</label>
+                                     <input type="file" class="form-control rounded" id="picture" name="picture">
                                     </div>
                                     @error('picture')
                                         <div class="error">* {{ $message }}</div>
                                     @enderror
                                     <!-- Tombol Submit -->
-                                    <button type="submit" class="btn btn-primary mt-4">Submit</button>
+                                    <button type="submit" class="btn btn-primary mt-4 move-right " style="background-color: #4c45b4; color: white;
+                                    ">Submit</button>
                                 </form>
                             </div>
                         </div>
